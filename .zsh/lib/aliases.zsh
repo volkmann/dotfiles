794f316549c53b63768baebdbdc1555afd794f00
Bjoern Volkmann <volkmann@pre-sense.de>
2019-02-18 13:04:06 +0100

    add and modify some zsh aliases

--- .zsh/lib/aliases.zsh
+++ .zsh/lib/aliases.zsh
@@ -2,6 +2,9 @@ alias ll='ls -la'
 alias lh='ls -lah'
 alias ...='cd ../..'
 alias now='date +%s'
-alias l='ls -lh'
+alias l='ls -lh --group-directories-first'
 alias rm='rm -i'
+alias mv='mv -i'
+alias cp='cp -i'
+alias xclip="xclip -selection clipboard"
 
